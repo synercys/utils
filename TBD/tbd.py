d3a753c55455fe00dbb62eb848daaac97930b8b2
Kyo Kim <kyokim751@gmail.com>
2024-04-09 22:56:40 +0000

    Added script to compute total bregman divergence

--- TBD/tbd.py
+++ TBD/tbd.py
@@ -9,14 +9,14 @@ def get_d(p1:type[GaussianMixture], p2:type[GaussianMixture], n_components:int):
     new_covars = p1.covariances_ + p2.covariances_
     for i in range(n_components):
         output += multivariate_normal.pdf(np.zeros(2), mean=new_means[i], cov=new_covars[i])
-    return 2*output
+    return output
 
 def get_tbd(p_r, p_q):
     n_components = p_r.n_components
     d_r = get_d(p_r, p_r, n_components)
     d_q = get_d(p_q, p_q, n_components)
-    d_rq = get_d(p_r, p_q, n_components)
-    return (d_r + d_q - d_rq)/np.sqrt(1+4*dq)
+    d_rq = 2*get_d(p_r, p_q, n_components)
+    return (d_r + d_q - d_rq)/np.sqrt(1+4*d_rq)
 
 if __name__ == '__main__':
     # Example data
@@ -36,5 +36,5 @@ if __name__ == '__main__':
     p_q.fit(Q)
 
     # Get TBD distance between the two shapes
-    print(get_tdb(p_r, p_q))
+    print(get_tbd(p_r, p_q))
 
